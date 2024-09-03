4d8b954a053bc8efd37f508df9c9cb83b12377e6
ladikpe <linuxdikpe@gmail.om>
2024-09-03 13:43:09 +0000

    changed index file

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 let products = [
-    { id: 1, name: 'Laptop', price: 1000 },
+    { id: 1, name: 'Laptop', price: 10000 },
     { id: 2, name: 'Smartphone', price: 500 },
     { id: 3, name: 'Tablet', price: 300 },
 ];
